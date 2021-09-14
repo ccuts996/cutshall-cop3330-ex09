cf1d8823ea012722e9075794171a502f8a190c75
ccuts996 <90644291+ccuts996@users.noreply.github.com>
2021-09-13 20:44:19 -0400

    Initial commit

---
+++ src/main/java/ex09.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ex09 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String length;
+        System.out.print(" What is the width of the ceiling? ");
+
+        length = input.next();
+
+        Integer i = Integer.parseInt(length);
+
+        String width;
+        System.out.print(" What is the width of the ceiling? ");
+
+        width = input.next();
+
+        Integer j = Integer.parseInt(width);
+
+        int area = i * j;
+        int spread = 350;
+        int numberofgallons = area % spread;
+        int gallonsneeded;
+        if (numberofgallons>0)
+        {
+            gallonsneeded = (area/spread) + 1;
+        }
+        else
+        {
+            gallonsneeded = area/spread;
+        }
+        System.out.print(" You will need " + gallonsneeded + "  gallons of paint to cover " + area + " square feet.");
+    }
+}
+
